1e1e27c50582d8e3854c6b1909a9ff6764660629
dongkey <dongkey-dev@gmail.com>
2020-09-03 11:56:44 +0900

    rm

--- investing_crawling.py
+++ investing_crawling.py
@@ -8,7 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from tqdm import tqdm
 
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+# options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument('disable-gpu')
 options.add_argument('user-agent=Mozilla/5.0(Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
